21776ff1d493a89edb6605c37cdd1ec1ca7914d1
cadu12359 <csantoslima03@gmail.com>
2018-12-16 20:48:22 -0200

    corrigindo a consulta para exclusão da reserva

--- apaga_reserva.php
+++ apaga_reserva.php
@@ -1,15 +1,11 @@
 <?php
 session_start();
 include "banco.php";
-
-	
-		if($_SESSION['data_reserva'] > date("y.m.d")){
+		
 			$sqlApaga = "DELETE FROM reserva WHERE 
 		    fk_usuario = {$_SESSION['id_usuario']}";
 
 	    	$resultadoApaga = mysqli_query($conexao, $sqlApaga);
-
-		}
 		
 	   
 	    //se existir, a variável global "$_SESSION['apagado']" é tida como verdadeiro
